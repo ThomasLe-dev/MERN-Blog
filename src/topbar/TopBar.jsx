9321af12340e3e19d8e75f875f09b34eca50b6b4
Hashirama <88428250+LBTZ7@users.noreply.github.com>
2022-01-28 22:11:43 +0700

    Init project

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,28 @@
+import "./topBar.css";
+
+export default function topbar() {
+  return(
+        <div className="top">
+          <div className="topLeft">
+            <i className="topIcon fab fa-facebook"></i>
+            <i className="topIcon fab fa-twitter-square"></i>
+            <i className="topIcon fab fa-linkedin"></i>
+            <i className="topIcon fab fa-instagram-square"></i>
+          </div>
+          <div className="topCenter">
+              <ul className="topList">
+                  <li className="topList-item">HOME</li>
+                  <li className="topList-item">ABOUT</li>
+                  <li className="topList-item">CONTACT</li>
+                  <li className="topList-item">WRITE</li>
+                  <li className="topList-item">LOGOUT</li>
+              </ul>
+          </div>
+          <div className="topRight">
+              <img className="topImg" src="./images/profile-img.jpg" alt="" />
+              <i className="topSearch-icon fas fa-search"></i>
+          </div>
+        </div>
+    ); 
+    
+}
